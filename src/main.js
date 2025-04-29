99e64a39a4b77d809d00034588b3056a28952fb4
nadaeldsokey112 <nadaeldesokey993@gmail.com>
2025-04-29 14:39:39 +0300

    update accordion

--- src/main.js
+++ src/main.js
@@ -9,7 +9,6 @@ import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
 
-import "@/assets/styles.css"
 import "@/assets/main.css"
 
 const vuetify = createVuetify({
